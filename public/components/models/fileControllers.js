2cc1d6506e39f911355abbbe2729a785d82b79b7
ConsonanteC <pipejimenez.jfjr@gmail.com>
2024-07-30 06:55:06 -0500

    index

--- public/components/models/fileControllers.js
+++ public/components/models/fileControllers.js
@@ -5,15 +5,16 @@ export class FileController {
         this.columNames = [];
         this.processFile();
     }
+    ;
     processFile() {
-        const lines = this.fileContent.split("/[\r\n]+/").filter(line => line.trim() !== "");
+        const lines = this.fileContent.split(/[\r\n]+/).filter(line => line.trim() !== '');
         if (lines.length > 0) {
-            this.columNames[0].split(",");
+            this.columNames = lines[0].split(',');
             this.data = lines.slice(1).map(line => {
-                const values = line.split(",");
+                const values = line.split(',');
                 const row = {};
                 this.columNames.forEach((colName, index) => {
-                    row[colName] = values[index] || "";
+                    row[colName] = values[index] || '';
                 });
                 return row;
             });
